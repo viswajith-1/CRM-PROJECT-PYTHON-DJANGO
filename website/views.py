17c8c3c1c3ee5f08c8d5fea46c8d4474e2906f90
viswajithsb <viswajithsaji@gmail.com>
2024-02-04 18:17:29 +0530

    worked till database

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -28,3 +29,20 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You've have been logged out")
     return redirect('home')
+
+def register_user(request):
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,"You've have been successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'regusr.html', {'form':form})
+    
+    return render(request, 'regusr.html', {'form':form})
